805eaaf57854561ecf8d040c2570d5045fb859bd
JesseySt <thejes@hotmail.nl>
2025-01-15 15:57:46 +0100

    fixed darkmode isseu

--- src/app/actions/getUserInfo.ts
+++ src/app/actions/getUserInfo.ts
@@ -1,30 +1,34 @@
-import { cache } from 'react'
-import { notFound } from 'next/navigation'
-import { headers } from 'next/headers'
+import { cache } from "react";
+import { notFound } from "next/navigation";
+import { headers } from "next/headers";
 
-export interface UserInformation { 
-    location: {
-        language: {
-            code: string,
-            name: string
-            native: string
-        },
-    }
+export interface UserInformation {
+  location: {
+    language: {
+      code: string;
+      name: string;
+      native: string;
+    };
+  };
 }
 
 const getUserInfo = cache(async () => {
-    const realIP = headers().get('x-real-ip');
+  const realIP = headers().get("x-real-ip");
 
-    try {
-        const apiKey = process.env.IPREGISTRY_API_KEY;
+  try {
+    const apiKey = process.env.IPREGISTRY_API_KEY;
 
-        const result = await fetch(`https://api.ipregistry.co/${realIP}?key=${apiKey}`);
+    const result = await fetch(
+      `https://api.ipregistry.co${
+        realIP ? "/" + realIP : ""
+      }?key=${apiKey}`
+    );
 
-        return result.json() as unknown as UserInformation;
-    } catch (err) {
-        console.error(err);
-        notFound();
-    }
+    return result.json() as unknown as UserInformation;
+  } catch (err) {
+    console.error(err);
+    notFound();
+  }
 });
 
 export default getUserInfo;
